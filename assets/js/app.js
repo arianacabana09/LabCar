5e0b1bc7d48a688b9ed97b8e2104f4221eae2294
Ariana Cabana <acabana.0904@gmail.com>
2017-05-29 17:58:28 -0500

    modificando geolocation

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ function initMap() {
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            infoWindow.setContent('Aqui estas');
             map.setCenter(pos);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
